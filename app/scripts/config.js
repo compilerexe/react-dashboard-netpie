954a3dfeb9fae1853460fea7582b24f354a9b84c
Compiler Exe <compilerexe@gmail.com>
2017-10-23 18:21:16 +0700

    change variable to json

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,7 +1,9 @@
-const Config = {}
+const Config = {
 
-Config.appId = 'React'
-Config.appKey = 'StXlRc85KwNYJD5'
-Config.appSecret = 'snjmdRNEwG6GuwTofpl6UxfGe'
-Config.topic = '/gearname/topic'
-Config.alias = 'html'
+  appId: 'React',
+  appKey: 'StXlRc85KwNYJD5',
+  appSecret: 'snjmdRNEwG6GuwTofpl6UxfGe',
+  topic: '/gearname/topic',
+  alias: 'html'
+
+}
